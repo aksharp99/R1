43ab9843a527b76d66d0c645c6bc4847ad6538cc
aksharp99 <aksharp99@gmail.com>
2019-09-19 22:06:14 -0400

    another commit

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ function Person(props) {
   );
 }
 
+var app = (
+  <div>
+  <Person name="Yolanda" age="35" />
+  <Person name="Akshar" age="27" />
+  </div>
+);
+
 ReactDOM.render(<Person name="Yolanda" age="35" />, document.querySelector('#p1'));
 //ReactDOM.render(<Person />, document.getElementById('#p1'))
